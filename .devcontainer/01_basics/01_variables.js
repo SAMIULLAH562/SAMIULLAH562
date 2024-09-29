97d5b669e24130a4d1b9da3b9fa4b9dbc09175d6
SAMIULLAH562 <samiullah562343@gmail.com>
2024-09-29 14:05:45 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail = "samiullah562343@google.com "
+var accountPassword = "12345"
+accountCity = Islamabad
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "saim222@gmail.com"
+accountPassword = " 212121"
+accountCity = "Muree"
+
+console.log(accountId);
+
+/*
+not use var
+bcz issue in block scope and factional scope
+
+*/
+
+console.table ([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,22 @@
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+
+
+let IsLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+
+// 0 => false
+// 1 => true
+// "" => true
+// "sami" => true
+
+
+let someNumber = 33
+
+let strinNumber = String(someNumber)
+console.log (strinNumber)
