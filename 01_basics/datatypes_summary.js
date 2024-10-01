3090f32dbe091204bebee870eaa77f630acb44ce
SAMIULLAH562 <samiullah562343@gmail.com>
2024-10-01 14:06:29 +0000

    datatype summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,32 @@
+// PRIMITIVE
+
+// 7 types:-
+
+// String,Number,Boolean,Null,Undefined,Symbol,BigInt
+
+// const score = "100"
+// const scoreValue = 100.05
+
+// const IsloggedIn = false
+// outsidetemp = "null"
+
+// let  userEmail;
+
+// const id = symbol("123")
+// const anotherId = symbol("123")
+
+
+// ************************************Non Primitive*********************************************
+
+// Reference (Non-Primitive)
+
+// Array objects function
+
+
+// const heroes ["spiderman", "batman", "harrypotter"]
+
+// mySlf = 
+{
+    name = sami.
+    age = "17"
+}
